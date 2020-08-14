54de9d1af9d57bd9ac45bee83935fbeb4f57b08a
khuyennth <khuyen.nguyen@anphabe.com>
2020-08-14 18:11:28 +0700

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,7 +45,7 @@
 
   jQuery(".header-slides").fullClip(
     {
-        images: ['assets/images/bg-cover-qua.jpg','assets/images/bg-cover-4.jpg','assets/images/bg-cover-2.jpg', 'assets/images/bg-cover-1.jpg','assets/images/bg-cover-qua.jpg'],
+        images: ['assets/images/bg-cover-1.jpg'],
         transitionTime: 2000,
         wait: 5000
     });
